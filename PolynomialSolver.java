b939b91dea0a46cd9be1269afb04e2577b1f5bd9
Srikanth116-s <Srikanthtechsonix@gmail.com>
2025-09-08 12:38:54 +0530

    Update ShamirSecret

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -1,50 +1,48 @@
 import java.io.FileReader;
-import java.util.*;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 
-public class PolynomialSolver {
+public class ShamirSecret {
     public static void main(String[] args) throws Exception {
-        JSONObject j = (JSONObject) new JSONParser().parse(new FileReader("data.json"));
-        int A = decode(j.get("A"));
-        int B = decode(j.get("B"));
-        System.out.println("Decoded A=" + A + ", B=" + B);
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first root: ");
-        int r1 = sc.nextInt();
-        System.out.print("Enter second root: ");
-        int r2 = sc.nextInt();
-        int C = A * (r1 * r2);
-        System.out.println("Polynomial: " + A + "x^2 + " + B + "x + " + C + " = 0");
-        System.out.println("COnstant C = " + C);
-        sc.close();
+        JSONArray testcases = (JSONArray) new JSONParser().parse(new FileReader("data.json"));
+
+        int caseNo = 1;
+        for (Object obj : testcases) {
+            JSONObject j = (JSONObject) obj;
+            JSONObject keys = (JSONObject) j.get("keys");
+            int k = ((Number) keys.get("k")).intValue();
+
+            JSONArray data = (JSONArray) j.get("data");
+
+            double[] x = new double[k];
+            double[] y = new double[k];
+            for (int i = 0; i < k; i++) {
+                JSONObject point = (JSONObject) data.get(i);
+                x[i] = ((Number) point.get("base")).doubleValue();
+                y[i] = ((Number) point.get("value")).doubleValue();
+            }
+
+            double secret = lagrangeInterpolation(0, x, y);
+            System.out.println("Case " + caseNo + " → Reconstructed Secret = " + Math.round(secret));
+            caseNo++;
+        }
     }
 
-    private static int decode(Object val) {
-        if (val instanceof Number)
-            return ((Number) val).intValue();
-        String s = val.toString().trim().toLowerCase();
-        Map<String, Integer> words = new HashMap<>();
-        words.put("zero", 0);
-        words.put("one", 1);
-        words.put("two", 2);
-        words.put("three", 3);
-        words.put("four", 4);
-        words.put("five", 5);
-        words.put("six", 6);
-        words.put("seven", 7);
-        words.put("eight", 8);
-        words.put("nine", 9);
-        if (words.containsKey(s))
-            return words.get(s);
-        try {
-            return Integer.parseInt(s);
-        } catch (Exception e) {
+    private static double lagrangeInterpolation(double at, double[] x, double[] y) {
+        double result = 0.0;
+        int n = x.length;
+
+        for (int i = 0; i < n; i++) {
+            double term = y[i];
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    term *= (at - x[j]) / (x[i] - x[j]);
+                }
+            }
+            result += term;
         }
-        try {
-            return Integer.decode(s);
-        } catch (Exception e) {
-        }
-        throw new IllegalArgumentException("Unsupported format: " + val);
+
+        return result;
     }
 }
